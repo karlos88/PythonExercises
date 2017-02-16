0f9ba0bfe348e3e9731a52122739d0d5fbe475f4
Karol <karlos88>
2017-02-16 21:46:22 +0100

    Make improvements
    - use class static fields for auto incrementing
    product and inventory id's when new is created
    - overload __str__ in Product for print purposes
    - remove redundant setters & getters
    (in first patch used just as a practice)
    
    Signed-off-by: Karol <karlos88>

--- ClassesExercises/ProductsInventory/ProductInventory.py
+++ ClassesExercises/ProductsInventory/ProductInventory.py
@@ -7,94 +7,62 @@ Then create an inventory class which keeps track of various products and can sum
 
 
 class Product(object):
-    def __init__(self, p_id, name, price, quantity=0):
-        self._id = p_id
-        self._name = name
-        self._price = price
-        self._quantity = quantity
+    p_id_cnt = 0
 
-    @property
-    def id(self):
-        return self._id
+    def __init__(self, name, price, quantity=0):
+        Product.p_id_cnt += 1
+        self.id = Product.p_id_cnt
+        self.name = name
+        self.price = price
+        self.quantity = quantity
 
-    @property
-    def name(self):
-        return self._name
-
-    @name.setter
-    def name(self, name):
-        self._name = name
-
-    @property
-    def price(self):
-        return self._price
-
-    @price.setter
-    def price(self, price):
-        self._price = price
-
-    @property
-    def quantity(self):
-        return self._quantity
-
-    @quantity.setter
-    def quantity(self, quantity):
-        self._quantity = quantity
+    def __str__(self):
+        return("{self.id} {self.name} {self.price} {self.quantity}".format(self=self))
 
 
 class Inventory(object):
-    def __init__(self, i_id):
-        self._inv_id = i_id
-        self._product_list = []
-        self._inv_value = 0
+    inv_id_cnt = 0
 
-    def add_product(self, p_id, name, price, quantity=0):
-        self._product_list.append(Product(p_id, name, price, quantity))
+    def __init__(self):
+        Inventory.inv_id_cnt += 1
+        self.id = Inventory.inv_id_cnt
+        self.products_list = []
+
+    def add_product(self, name, price, quantity=0):
+        self.products_list.append(Product(name, price, quantity))
 
     def del_product_by_id(self, p_id):
-        for product in self.products:
+        for product in self.products_list:
             if product.id == p_id:
-                self.products.remove(product)
+                self.products_list.remove(product)
 
     def del_product_by_name(self, name):
-        for product in self.products:
+        for product in self.products_list:
             if product.name in name:
-                self.products.remove(product)
+                self.products_list.remove(product)
 
     def print_products(self):
-        for product in self.products:
-            print("{p_name} {p_id} {p_price} {p_quantity}".format(
-                p_name=product.name, p_id=product.id,
-                p_price=product.price, p_quantity=product.quantity
-            ))
+        for product in self.products_list:
+            print(product)
 
     def count_inv_value(self):
-        value = 0
-        for pr in self.products:
-            value += pr.quantity * pr.price
-        return value
-        # Other way:
-        # return sum([pr.quantity * pr.price for pr in self.products])
+        return sum(pr.quantity * pr.price for pr in self.products_list)
 
     def count_product_types(self):
-        return len(self.products)
+        return len(self.products_list)
 
     def count_all_products(self):
-        return sum([pr.quantity for pr in self.products])
-
-    @property
-    def products(self):
-        return self._product_list
+        return sum(pr.quantity for pr in self.products_list)
 
 if __name__ == "__main__":
 
-    inv = Inventory(1)
+    inv = Inventory()
 
-    inv.add_product(1, "Butter", 5, 10)
-    inv.add_product(2, "Bread", 1)
-    inv.add_product(2, "Duplicate Bread", 3, 10)
-    inv.add_product(4, "DELETE", 7, 7)
-    inv.add_product(5, "DELETE", 7, 7)
+    inv.add_product("Butter", 5, 10)
+    inv.add_product("Bread", 1)
+    inv.add_product("Duplicate Bread", 3, 10)
+    inv.add_product("DELETE", 7, 7)
+    inv.add_product("DELETE", 10, 7)
 
     print("Products list:")
     inv.print_products()
